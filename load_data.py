2a0fe78bbca82511fcbdd88a72bcf3f85cbad976
cactus3106@gmail.com <anton3105@bk.ru>
2026-01-29 14:44:39 +0300

    rewrite _fetch_market_data() internal function

--- load_data.py
+++ load_data.py
@@ -1,33 +1,24 @@
 import ccxt
 import pandas as pd
-
 from langchain_core.tools import tool
 
 
-@tool
-def fetch_market_data(symbol="BTC/USDT", timeframe='1h', limit=100):
-    """
-    Loads data from MEXC exchange
-    
-    :param symbol: exchange pair
-    :param timeframe: what timeframe candles are indicating
-    :param limit: limit of last candles
-    """
+# вспомогательная функция для загрузки данных
+def _fetch_market_data(symbol="BTC/USDT", timeframe='1h', limit=100):
     exchange = ccxt.mexc()
 
-    try:
-        print(f"Загружаю данные для {symbol} {timeframe}")
+    # делаем запрос к бирже: Open, High, Low, Close, Volume
+    ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
+    df = pd.DataFrame(ohlcv, columns=['timestamp', 'open', 'high',
+                                       'low', 'close', 'volume'])
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
 
-        # делаем запрос к бирже: Open, High, Low, Close, Volume
-        ohlcv = exchange.fetch_ohlcv(symbol, timeframe, limit=limit)
-        return ohlcv
+    return df
 
-    except Exception as e:
-        print(f"Error: {e}")
-        return None
+
+
     
 
-@tool
 def count_sma(df: pd.DataFrame, candles_cnt=50) -> pd.Series:
     """
     average price for last [candles_cnt] candles
@@ -36,7 +27,6 @@ def count_sma(df: pd.DataFrame, candles_cnt=50) -> pd.Series:
 
     return df['close'].rolling(window=candles_cnt).mean()
 
-@tool
 def count_rsi(df: pd.DataFrame, candles_cnt=14) -> pd.Series:
     """
     counts RSI indicator for last [candles_cnt] candles
@@ -53,6 +43,14 @@ def count_rsi(df: pd.DataFrame, candles_cnt=14) -> pd.Series:
     
     return 100 - (100 / (1 + rs))
         
+
+
+
+@tool
+def get_technical_analysis(symbol: str,
+                           timeframe : str = '1h'):
+    
+
 
 if __name__ == "__main__":
     data = fetch_market_data(timeframe='1m')
